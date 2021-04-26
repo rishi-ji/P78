63f36ee3972df9da5774609f3108ddf36415c2f9
rishi-ji <66062196+rishi-ji@users.noreply.github.com>
2021-04-26 09:06:05 +0530

    Add files via upload

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,22 @@
+/*import React, { Component } from 'react';
+import {Image} from "react-native"
+import {createBottomTabNavigator} from "react-navigation-tabs"
+import BookDonateScreen from "../screens/BookDonateScreen"
+import BookRequestScreen from "../screens/BookRequestScreen"
+
+export const AppTabNavigator=createBottomTabNavigator({
+    DonateBooks:{
+        screen:BookDonateScreen,
+        navigationOptions:{
+            tabBarIcon:<Image source={require("../assets/request-list.png")} style={{width:20,height:20}}/>,
+            tabBarLabel:"donate books"
+        }
+    },
+    RequestBooks:{
+        screen:BookRequestScreen,
+        navigationOptions:{
+            tabBarIcon:<Image source={require("../assets/request-book.png")} style={{width:20,height:20}}/>,
+            tabBarLabel:"request books"
+        }
+    },
+})*/
